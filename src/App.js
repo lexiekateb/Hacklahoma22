628780f93ea227e38bc4a57ec3b6c524a37df9a4
YarbroughJaveon <Javeon.A.Yarbrough-1@ou.edu>
2022-02-12 17:18:31 -0600

    javeon's Thang

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.png';
+import User from './User.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+          <header className="App-header">
+              <body>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Create your profile below!</h1>
+                  <h1> Create your profile below!</h1>
+        <img src={User} className="User" alt="User" />
+              </body>
       </header>
     </div>
   );
