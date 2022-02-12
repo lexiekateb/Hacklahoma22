728bb553ca38da9a9d6e2d843be0524d09c08582
Lexie Barthelemess <lexieb@ou.edu>
2022-02-12 14:37:10 -0600

    Initialize title

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< HEAD
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Bananas grow on trees</h1>
+=======
+>>>>>>> 25deb2a (Initialize title)
         <p>
-          This is the start of our Hacklahoma project
+          Welcome to Find my Friend!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
