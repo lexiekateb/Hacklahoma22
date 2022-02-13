30ef82e6ce0a955a75658e3599aa3b75eb05014f
Lexie Barthelemess <lexieb@ou.edu>
2022-02-13 10:36:44 -0600

    Final push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import logo from './logo.png';
+import button from './plus-button.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div class Name="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Create your profile below!</h1>
       </header>
+	<p> Click the button to add your profile. </p>
     </div>
   );
 }
+
+var butto =
+document.createElement("butto");
+butto.innerHTML = "Create Profile"
+
+var body =
+document.getElementsByTagName("body") [0];
+body.appendChild(butto);
+
+butto.addEventListener ("click",
+function() {
+  alert("Enter your information below:");
+});
 
 export default App;
